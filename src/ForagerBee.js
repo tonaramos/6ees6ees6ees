9088b6318afacbe4c56acccb6ce2196e37cefc17
Gabriel Tonatiuh Ramos Vilchis <Tona-@Macbook-Pro-2.local>
2018-05-09 13:08:03 -0700

    added implementation using es6 to all bees classes

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
+class ForagerBee extends HoneyMakerBee {
+	constructor () {
+		super ();
+		this.age = 10;
+		this.job = 'find pollen';
+		this.canFly = true;
+		this.treasureChest = [];
+	}
+	forage (item) {
+      this.treasureChest.push (item);
+	}
   // TODO..
 };
